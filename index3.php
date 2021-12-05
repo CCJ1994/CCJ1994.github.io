c6b80259be58747a930bc036d4acef05140043b1
CCJ1994 <a0changj2@gmail.com>
2021-12-05 21:16:00 +0800

    101eip

--- index3.php
+++ index3.php
@@ -19,6 +19,7 @@
   let slide = pres.addSlide();
 
   // 3. Add one or more objects (Tables, Shapes, Images, Text and Media) to the Slide
+  
   // 標題
   slide.addText("xxx人選推薦", {
     x: 1,
@@ -187,17 +188,11 @@
     fill: "#34495e",
   });
   // 簡介
-  // slide.addText("", {
-  //   x: 1,
-  //   y: 2,
-  //   w: 2.7,
-  //   h: 1.3,
-  //   fill: "f3f7fa",
-  // });
+  
   slide.addText("ABOUT", {
     x: 0.95,
-    y: 2.8,
-    w: 2.8,
+    y: 3.1,
+    w: 2.7,
     h: 0.5,
     fontFace: "Microsoft JhengHei",
     bold:true,
@@ -208,7 +203,7 @@
   slide.addText("簡介", {
     x: 0.95,
     y: 2,
-    w: 1,
+    w: 2.7,
     h: 0.3,
     bold:true,
     color: "#34495e",
@@ -218,23 +213,17 @@
   slide.addText("劉芒從模特兒轉戰YouTuber，她爽朗又幽默的個性造就影片的浮誇風格，也因為那些浮誇感，讓影片充滿了「笑果」，讓人印象深刻。", {
     x: 0.95,
     y: 2,
-    w: 2.8,
-    h: 1.25,
+    w: 2.7,
+    h: 1.4,
     color: "#4b6584",
     fontFace: "Microsoft JhengHei",
     fontSize: 11
   });
   // 推薦
-  // slide.addText("", {
-  //   x: 1,
-  //   y: 3.4,
-  //   w: 2.7,
-  //   h: 1.7,
-  //   fill: "f3f7fa",
-  // });
+
   slide.addText("RECOMMEND", {
-    x: 0.95,
-    y: 4.6,
+    x: 0.9,
+    y: 4.7,
     w: 2.8,
     h: 0.5,
     fontFace: "Microsoft JhengHei",
@@ -245,8 +234,8 @@
   });
   slide.addText("推薦原因", {
     x: 0.95,
-    y: 3.4,
-    w: 1,
+    y: 3.5,
+    w: 2.7,
     h: 0.3,
     bold:true,
     color: "#34495e",
@@ -255,84 +244,37 @@
   });
   slide.addText("怪奇事務所，透過具有識別性的插圖，巧妙地將生活事物與冷知識相連結，趣味的圖文內容吸引大批粉絲長期關注。透過他的知名度來宣傳本產品，能增加討論度，也能宣導大家重視洗眼睛的重要性。", {
     x: 0.95,
-    y: 3.6,
-    w: 2.8,
-    h: 1.25,
+    y: 3.7,
+    w: 2.7,
+    h: 1.35,
     color: "#4b6584",
     fontFace: "Microsoft JhengHei",
     fontSize: 11
   });
   //圖1
   slide.addText("", {
-    x: 3.8,
+    x: 3.6,
     y: 2,
-    w: 1.5,
-    h: 2.3,
+    w: 2.7,
+    h: 3.2,
     fill: "f3f7fa",
-  });
-  slide.addText("圖片標題", {
-    x: 3.75,
-    y: 2,
-    w: 1,
-    h: 0.3,
-    color: "#34495e",
-    bold:true,
-    fontFace: "Microsoft JhengHei",
-    fontSize: 12
   });
   slide.addImage({
     path: "./img/get_pic64.php.jpeg",
-    x: 3.8,
-    y: 2.3,
+    x: 3.6,
+    y: 2,
+    w:3.7,
+    h:3.7,
     sizing: {
-      type: "cover",
-      w: 1.5,
-      h: 2
+      type: "crop",
+      w: 2.7,
+      h: 2.7,
+      x:0.5,
+      y:0.5
     }
   });
-  slide.addText("", {
-    x: 3.8,
-    y: 4.4,
-    w: 1.5,
-    h: 0.7,
-    fill: "f3f7fa",
-  });
-  slide.addText("預估圖文互動數", {
-    x: 3.75,
-    y: 4.4,
-    w: 1.6,
-    h: 0.3,
-    color: "#34495e",
-    bold:true,
-    fontFace: "Microsoft JhengHei",
-    fontSize: 12
-  });
-  slide.addText("9,000", {
-    x: 3.75,
-    y: 4.7,
-    w: 1.6,
-    h: 0.3,
-    color: "#34495e",
-    fontFace: "Microsoft JhengHei",
-    fontSize: 18
-  });
-  //圖2
-  slide.addText("", {
-    x: 5.4,
-    y: 2,
-    w: 2.5,
-    h: 0.9,
-    fill: "f3f7fa",
-  });
-  slide.addText("", {
-    x: 5.4,
-    y: 3,
-    w: 2.5,
-    h: 2.1,
-    fill: "f3f7fa",
-  });
   slide.addText("圖片標題", {
-    x: 7,
+    x: 3.6,
     y: 4.8,
     w: 1,
     h: 0.3,
@@ -341,32 +283,21 @@
     fontFace: "Microsoft JhengHei",
     fontSize: 12
   });
-  slide.addImage({
-    path: "./img/get_pic64.php-2.jpeg",
-    x: 5.4,
-    y: 3,
-    sizing: {
-      type: "cover",
-      w: 2.5,
-      h: 1.8
-    }
-  });
+  
 
-  //圖3
-  slide.addText("STYLE", {
-    x: 7.95,
+  
+  //圖2
+  slide.addText("", {
+    x: 6.4,
     y: 2.5,
     w: 1.6,
-    h: 0.5,
-    fontFace: "Microsoft JhengHei",
-    bold:true,
-    color: "f3f4f5",
-    align:"right",
-    fontSize: 27,
+    h: 1.9,
+    fill: "f3f7fa",
   });
-  slide.addText("圖文風格", {
-    x: 7.95,
-    y: 2,
+  
+  slide.addText("圖片標題", {
+    x: 6.4,
+    y: 4.1,
     w: 1,
     h: 0.3,
     color: "#34495e",
@@ -374,25 +305,118 @@
     fontFace: "Microsoft JhengHei",
     fontSize: 12
   });
-  slide.addText("https://www.instagram.com/p/CS4VaHdhvKO/?utm_source=ig_web_copy_link", {
-    x: 7.95,
-    y: 2.3,
+  slide.addImage({
+    path: "./img/get_pic64.php.jpeg",
+    x: 6.4,
+    y: 2.5,
+    w:2.2,
+    h:2.2,
+    sizing: {
+      type: "crop",
+      w: 1.6,
+      h: 1.6,
+      x:0.3,
+      y:0.3
+    }
+  });
+  slide.addText("", {
+    x: 6.4,
+    y: 4.5,
     w: 1.6,
+    h: 0.7,
+    fill: "f3f7fa",
+  });
+  slide.addText("預估圖文互動數", {
+    x: 6.4,
+    y: 4.5,
+    w: 1.6,
+    h: 0.3,
+    color: "#34495e",
+    bold:true,
+    fontFace: "Microsoft JhengHei",
+    fontSize: 12
+  });
+  slide.addText("9,000", {
+    x: 6.4,
+    y: 4.8,
+    w: 1.6,
+    h: 0.3,
+    align:"center",
+    color: "#34495e",
+    fontFace: "Microsoft JhengHei",
+    fontSize: 18
+  });
+
+
+  slide.addText("", {
+    x: 6.4,
+    y: 2,
+    w: 3.1,
+    h: 0.4,
+    fill: "f3f7fa",
+  });
+  slide.addText("STYLE", {
+    x: 8,
+    y: 3,
+    w: 1.5,
     h: 0.5,
+    fontFace: "Microsoft JhengHei",
+    bold:true,
+    color: "f3f4f5",
+    align:"right",
+    fontSize: 27,
+  });
+  
+  slide.addText("https://www.instagram.com/p/CS4VaHdhvKO/?utm_source=ig_web_copy_link", {
+    x: 8,
+    y: 2.45,
+    w: 1.4,
+    h: 1.1,
     color: "#34495e",
     fontFace: "Microsoft JhengHei",
     fontSize: 8,
     hyperlink:{url:'https://www.instagram.com/p/CS4VaHdhvKO/?utm_source=ig_web_copy_link'}
-
+  });
+  slide.addText("xx風格", {
+    x: 8,
+    y: 2.45,
+    w: 1,
+    h: 0.3,
+    color: "#34495e",
+    bold:true,
+    fontFace: "Microsoft JhengHei",
+    fontSize: 12
+  });
+  //圖3
+  slide.addText("", {
+    x: 8.1,
+    y: 3.5,
+    w: 1.4,
+    h: 1.6,
+    fill: "f3f7fa",
+  });
+  slide.addText("圖片標題", {
+    x: 8.1,
+    y: 3.5,
+    w: 1,
+    h: 0.3,
+    color: "#34495e",
+    bold:true,
+    fontFace: "Microsoft JhengHei",
+    fontSize: 12
   });
   slide.addImage({
     path: "./img/get_pic64.php.jpeg",
-    x: 8,
-    y: 3.1,
+    x: 8.1,
+    y: 3.8,
+    w:1.9,
+    h:1.9,
     sizing: {
-      type: "cover",
-      w: 1.5,
-      h: 2
+      type: "crop",
+      w: 1.4,
+      h: 1.4,
+      x:0.27,
+      y:0.27
     }
   });
   
